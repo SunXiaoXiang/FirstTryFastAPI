e558b1aa0784180d3f69d0d2d1d62e35f7fcafbd
sunxiaoxiang <hereaream@gmail.com>
2024-11-11 15:12:54 +0000

    modify ruff

--- simple.py
+++ simple.py
@@ -8,5 +8,5 @@ async def HelloWorld():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-    async def read_item(item_id: int):
+async def read_item(item_id: int):
     return {"item_id": item_id}
