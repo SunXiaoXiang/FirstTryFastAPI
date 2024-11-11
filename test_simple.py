8eddcc256199986aecfb1ed4665bba4a06bbdaa4
sunxiaoxiang <hereaream@gmail.com>
2024-11-11 14:21:14 +0000

    add simple test fastapi helloworld

--- test_simple.py
+++ test_simple.py
@@ -1,5 +1,6 @@
 import sys
-
+from fastapi.testclient import TestClient
+from simple import app
 
 def test_sys_version(capsys):
     # 调用 print(sys.version)
@@ -10,3 +11,10 @@ def test_sys_version(capsys):
 
     # 断言输出是否符合预期
     assert captured.out.strip() == sys.version.strip()
+
+client = TestClient(app)
+
+def test_HelloWorld():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
