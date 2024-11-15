24d96be31fd4958334afeee6e70c9eb1ae322068
SunXiaoXiang <hereaream@gmail.com>
2024-11-15 15:25:39 +0000

    udpate requestbody3

--- test_requestBody3.py
+++ test_requestBody3.py
@@ -6,30 +6,38 @@ client = TestClient(app)
 
 # 测试PUT请求，传入有效的item_id
 def test_read_item_valid_id():
-    response = client.put("/items/", json={"item_id": 1})
+    response = client.put("/items/", json=1)
     print(f"Response JSON: {response.json()}")
-    assert response.status_code == 422
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 1}
     #assert response.json() == {"detail":[{"loc":["string",0],"msg":"string","type":"int_type"}]}
     
 
 # 测试PUT请求，传入无效的item_id（例如非整数）
 def test_read_item_invalid_id():
-    response = client.put("/items/", json={"item_id": "not_an_int"})
+    response = client.put("/items/", json="not_an_int")
+    print(f"Response JSON: {response.json()}")
     assert response.status_code == 422  # 验证错误应返回422 Unprocessable Entity
+    assert response.json() == {'detail': [{'type': 'int_parsing', 'loc': ['body'], 'msg': 'Input should be a valid integer, unable to parse string as an integer', 'input': 'not_an_int'}]}
 
 # 测试PUT请求，不传入item_id
 def test_read_item_missing_id():
     response = client.put("/items/", json={})
+    print(f"Response JSON: {response.json()}")
     assert response.status_code == 422  # 验证错误应返回422 Unprocessable Entity
+    assert response.json() == {'detail': [{'type': 'int_type', 'loc': ['body'], 'msg': 'Input should be a valid integer', 'input': {}}]}
+
 
 # 测试PUT请求，传入负数的item_id
 def test_read_item_negative_id():
-    response = client.put("/items/", json={"item_id": -1})
-    assert response.status_code == 422  # 负数id仍然会被接受，但实际应用中可能需要进一步处理
-    #assert response.json() == {"item_id": -1}
+    response = client.put("/items/", json=-1)
+    print(f"Response JSON: {response.json()}")
+    assert response.status_code == 200  # 负数id仍然会被接受，但实际应用中可能需要进一步处理
+    assert response.json() == {"item_id": -1}
 
 # 测试PUT请求，传入非常大的item_id
 def test_read_item_large_id():
-    response = client.put("/items/", json={"item_id": 9999999999999999999})
-    assert response.status_code == 422  # 非常大的id仍然会被接受，但实际应用中可能需要进一步处理
-    #assert response.json() == {"item_id": 9999999999999999999}
+    response = client.put("/items/", json=9999999999999999999)
+    print(f"Response JSON: {response.json()}")
+    assert response.status_code == 200  # 非常大的id仍然会被接受，但实际应用中可能需要进一步处理
+    assert response.json() == {"item_id": 9999999999999999999}
